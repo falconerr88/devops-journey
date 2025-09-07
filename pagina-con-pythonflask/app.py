d9f78000311f26462075748bf42b3acda0bf38a3
falconerr88 <thiagolovey1@gmail.com>
2025-09-07 17:11:45 -0300

    agregando la app con Python Flask

---
+++ pagina-con-pythonflask/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return( 
+        '<h1> App con Flask para aprender docker </h1>'
+        '<h2> Desarrollado por: Thiago Lovey 7/9/25 </h2> <p1> Container 3 funcionando </p1>'
+    )
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
